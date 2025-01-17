0fc84a274d8337055e5d123ea0276daee20bfe59
andrewdo <ado3@luc.edu>
2025-01-16 22:57:59 -0600

    Initial setup

---
+++ config/settings.py
@@ -0,0 +1,12 @@
+# config/settings.py
+
+# LinkedIn URLs
+LINKEDIN_LOGIN_URL = "https://www.linkedin.com/login"
+LINKEDIN_JOBS_URL = "https://www.linkedin.com/jobs/search/"
+
+# LinkedIn Login Credentials
+USERNAME = "your_email@example.com"
+PASSWORD = "your_password"
+
+# Scraper Settings
+SCROLL_PAUSE_TIME = 2  # Pause time (in seconds) for scrolling
